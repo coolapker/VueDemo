dfc84be3738270b941a9ec3cf727db5b9db5f87b
mojospy <1413293308@qq.com>
2022-07-07 22:35:35 +0800

    add vue, element-ui  dependencies

--- Test/axiosTest.js
+++ Test/axiosTest.js
@@ -1,6 +1,6 @@
 // import axios from 'axios';
 const axios = require('axios').default;
-axios.get("http://localhost:8080/movies/1").then((data,status)=>{
-    console.log(data.data);
-    console.log(status);
+axios.get("http://localhost:8080/movies/1").then((data)=>{
+    // console.log(data);
+    console.log(data);
 })
