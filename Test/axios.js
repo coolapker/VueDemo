0a73b77a6dc4c13c0b9ae30c162e8717ada52ff8
mojospy <1413293308@qq.com>
2022-07-07 18:15:30 +0800

    first commit

---
+++ Test/axios.js
@@ -0,0 +1,6 @@
+// import axios from 'axios';
+const axios = require('axios').default;
+axios.get("http://localhost:8080/movies/1").then(function(data,status){
+    console.log(data.data);
+    console.log(status);
+})
