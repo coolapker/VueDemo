c3ba6ebfeaf89758b8ccda75a8d8348d7b001c94
mojospy <1413293308@qq.com>
2022-07-08 21:26:07 +0800

    test

--- Test/axiosTest.js
+++ Test/axiosTest.js
@@ -1,2 +1,6 @@
 let axios = require('axios');
-axios.get("http://localhost:8080/movies/1").then(code=>{console.log(code.data);} )
+axios.get("http://localhost:8080/movies/1").then((result) => {
+    
+}).catch((err) => {
+    console.log(err.);
+});
